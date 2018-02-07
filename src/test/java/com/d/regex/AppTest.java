198f544b2fe19c5dfff277021eaa6f3979efdb2a
Dxh7788 <anan1215>
2018-02-07 14:56:59 +0800

    first commit

---
+++ src/test/java/com/d/regex/AppTest.java
@@ -0,0 +1,18 @@
+package com.d.regex;
+
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
